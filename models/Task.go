7b9633da42b7e25711057078ee0553dd145c957b
Denis S. Runov <denis_runov@prof-p.ru>
2020-07-04 12:00:03 +0300

    Изменения в Task

--- models/Task.go
+++ models/Task.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 // Task - Задачи для запуска
-type Tasks struct {
-	TSID int `gorm:"column:ts_id;primary_key"`
-	TSName string `gorm:"column:ts_name"`
+type Task struct {
+	TSID        int       `gorm:"column:ts_id;primary_key"`
+	TSName      string    `gorm:"column:ts_name"`
 	TSDataStart time.Time `gorm:"column:ts_data_start"`
-	TSRunTimes int `gorm:"column:ts_run_times"`
-	TSComment string `gorm:"column:ts_comment"`
+	TSRunTimes  int       `gorm:"column:ts_run_times"`
+	TSComment   string    `gorm:"column:ts_comment"`
 }
